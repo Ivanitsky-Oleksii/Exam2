14d4c8a934bbcc533ff5db3cca916a08ee4e51e3
Ivanitskyi Oleksii <goleecindj@gmail.com>
2023-03-06 17:14:25 +0200

    add home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,14 @@
-import ProductList from "components/ProductList/ProductList"
-import Reviews from "components/Reviews/Reviews"
+
 
 const Home = () => {
 return (
+    <>
     <div>
-<ProductList/>
-<Reviews/>
+
 
 
     </div>
+    </>
 )
 }
 export default Home
